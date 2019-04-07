21c7fd35499b9198790705941a25d8e9db748a29
Saurav Guin <saurav.guin1989@gmail.com>
2019-04-07 09:28:32 +0530

    Added Add method

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        public int Add()
+        {
+            int x = 1;
+            int y = 2;
+            int sum = x + y;
+            return sum;
+        }
     }
 }
